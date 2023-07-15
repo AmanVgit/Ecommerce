7909bcba982af815c19910f87389cbb491f3f591
Aman verma <9.amanverma1@gmail.com>
2023-07-16 03:33:39 +0530

    cart pannel and search

--- client/src/components/Cart/CartItem/CartItem.jsx
+++ client/src/components/Cart/CartItem/CartItem.jsx
@@ -1,6 +1,33 @@
 import "./CartItem.scss";
+import React, { useContext } from "react";
+import prod from "../../../assets/products/earbuds-prod-1.webp"
+import { Context } from "../../../utils/context";
+import { MdClose } from "react-icons/md";
+
 const CartItem = () => {
-    return <div>Cart item</div>;
+    return (
+        <div className="cart-products">
+            <div className="cart-product">
+                <div className="img-container">
+                    <img src={prod} alt="" />
+                </div>
+                <div className="prod-details">
+                    <span className="name">Product name</span>
+                    <MdClose className="close-btn"/>
+                    <div className="quantity-buttons">
+                        <span>-</span>
+                        <span>5</span>
+                        <span>+</span>
+                    </div>
+                    <div className="text">
+                        <span>3</span>
+                        <span>x</span>
+                        <span>&#8377;1234</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default CartItem;
--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -12,6 +12,8 @@ import Cart from "../Cart/Cart";
 const Header = () => {
 
     const [scrolled, setScrolled] = useState(false);
+    const [showCart, setShowCart] = useState(false);
+    const [showSearch, setShowSearch] = useState(false);
     const [searchModal, setSearchModal] = useState(false);
     const navigate = useNavigate();
     const handleScroll = () => {
@@ -27,23 +29,30 @@ const Header = () => {
         window.addEventListener("scroll", handleScroll);
     }, []);
 
-    return <header className={`main-header ${scrolled ? "sticky-header" : ""}`}>
-        <div className="header-content">
-            <ul className="left">
-                <li>Home</li>
-                <li>About</li>
-                <li>Categories</li>
-            </ul>
-            <div className="center">ShopperSpy</div>
-            <div className="right">
-                <TbSearch />
-                <AiOutlineHeart />
-                <span className="cart-icon">
-                    <CgShoppingCart />
-                </span>
-            </div>
-        </div>
-    </header>;
+    return (
+        <>
+            <header className={`main-header ${scrolled ? "sticky-header" : ""}`}>
+                <div className="header-content">
+                    <ul className="left">
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Categories</li>
+                    </ul>
+                    <div className="center">ShopperSpy</div>
+                    <div className="right">
+                        <TbSearch onClick={()=> setShowSearch(true)}/>
+                        <AiOutlineHeart />
+                        <span className="cart-icon" onClick={()=> setShowCart(true)}>
+                            <CgShoppingCart />
+                            <span>5</span>
+                        </span>
+                    </div>
+                </div>
+            </header>
+            {showCart && <Cart setShowCart={setShowCart}/>}
+            {showSearch && <Search setShowSearch={setShowSearch}/>}
+        </>
+    )
 };
 
 export default Header;
