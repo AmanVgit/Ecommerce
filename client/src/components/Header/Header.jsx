62f74a4debef7388b219b9c185e1fac1b6a2ccea
Aman verma <9.amanverma1@gmail.com>
2023-07-18 01:33:12 +0530

    Product details page API integration

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -34,11 +34,11 @@ const Header = () => {
             <header className={`main-header ${scrolled ? "sticky-header" : ""}`}>
                 <div className="header-content">
                     <ul className="left">
-                        <li>Home</li>
+                        <li onClick={()=> navigate("/")}>Home</li>
                         <li>About</li>
                         <li>Categories</li>
                     </ul>
-                    <div className="center">ShopperSpy</div>
+                    <div className="center" onClick={()=> navigate("/")}>ShopperSpy</div>
                     <div className="right">
                         <TbSearch onClick={()=> setShowSearch(true)}/>
                         <AiOutlineHeart />
--- client/src/components/SingleProduct/SingleProduct.jsx
+++ client/src/components/SingleProduct/SingleProduct.jsx
@@ -11,55 +11,92 @@ import {
     FaPinterest,
     FaCartPlus,
 } from "react-icons/fa";
-import prod from "../../assets/products/earbuds-prod-1.webp"
 import "./SingleProduct.scss";
 
 const SingleProduct = () => {
-    return ( <div className="single-product-main-content">
-        <div className="layout">
-            <div className="single-product-page">
-                <div className="left">
-                    <img src={prod} alt="" />
-                </div>
-                <div className="right">
-                    <span className="name">Product name</span>
-                    <span className="price">Price</span>
-                    <span className="desc">Description</span>
+    const [quantity, setQuantity] = useState(1);
+    const { id } = useParams();
+    const { handleAddToCart } = useContext(Context);
+    const { data } = useFetch(`/api/products?populate=*&[filters][id]=${id}`);
 
-                    <div className="cart-buttons">
-                        <div className="quantity-buttons">
-                            <span>-</span>
-                            <span>5</span>
-                            <span>+</span>
+    const decrement = () => {
+        setQuantity((prevState) => {
+            if (prevState === 1) return 1;
+            return prevState - 1;
+        });
+    };
+    const increment = () => {
+        setQuantity((prevState) => prevState + 1);
+    };
+
+    if (!data) return;
+    const product = data?.data?.[0]?.attributes;
+
+    return (
+        <div className="single-product-main-content">
+            <div className="layout">
+                <div className="single-product-page">
+                    <div className="left">
+                        <img
+                            src={
+                                process.env.REACT_APP_DEV_URL +
+                                product.img.data[0].attributes.url
+                            }
+                        />
+                    </div>
+                    <div className="right">
+                        <span className="name">{product.title}</span>
+                        <span className="price">&#8377;{product.price}</span>
+                        <span className="desc">{product.description}</span>
+
+                        <div className="cart-buttons">
+                            <div className="quantity-buttons">
+                                <span onClick={decrement}>-</span>
+                                <span>{quantity}</span>
+                                <span onClick={increment}>+</span>
+                            </div>
+                            <button
+                                className="add-to-cart-button"
+                                onClick={() => {
+                                    handleAddToCart(data?.data?.[0], quantity);
+                                    setQuantity(1);
+                                }}
+                            >
+                                <FaCartPlus size={20} />
+                                ADD TO CART
+                            </button>
                         </div>
-                        <button className="add-to-cart-button">
-                            <FaCartPlus size={20} />
-                            Add To Cart
-                        </button>
-                    </div>
 
-                    <span className="divider" />
-                    <div className="info-item">
-                        <span className="text-bold">
-                            Category:
-                            <span>Headphones</span>
-                        </span>
-                        <span className="text-bold">
-                            Share:
-                            <span className="social-icons">
-                                <FaFacebookF />
-                                <FaInstagram />
-                                <FaLinkedinIn />
-                                <FaPinterest />
-                                <FaTwitter />
+                        <span className="divider" />
+                        <div className="info-item">
+                            <span className="text-bold">
+                                Category:{" "}
+                                <span>
+                                    {
+                                        product.categories.data[0].attributes
+                                            .title
+                                    }
+                                </span>
                             </span>
-                        </span>
+                            <span className="text-bold">
+                                Share:
+                                <span className="social-icons">
+                                    <FaFacebookF size={16} />
+                                    <FaTwitter size={16} />
+                                    <FaInstagram size={16} />
+                                    <FaLinkedinIn size={16} />
+                                    <FaPinterest size={16} />
+                                </span>
+                            </span>
+                        </div>
                     </div>
                 </div>
+                <RelatedProducts
+                    productId={id}
+                    categoryId={product.categories.data[0].id}
+                />
             </div>
-            <RelatedProducts />
         </div>
-    </div>
     );
 };
 
